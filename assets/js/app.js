f79b74391964126f959a44e88f236ba64a32533a
Rafael Uribe <72708040+rafaelUribe@users.noreply.github.com>
2022-10-28 12:26:06 -0500

    app fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,34 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.getElementById('name');
+const $b = document.getElementById('blog');
+const $l = document.getElementById('location');
+// se cambiaron los metodos a get element by id para manejarlos individualemnte
 
-function displayUser(username) {
+// se usa async para indicar que se va a trabajar con promesas
+const displayUser = async (username) => {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  // se usaron bloques de try catch
+  try{
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json()
+    // se uso el metodo .json() para guardar los datos en variabla
+    console.log(data);
+    $n.textContent = `${data.name}`;
+    // se cambiaron las comillas para poder imprimir los datos
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
+  }
+  catch(e){
+    handleError(e)
+  }
+
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
